1644eb99dcb22915288be7101771c173c7ce790d
sahil_mujawar <mujawarmsahil@gmail.com>
2025-03-12 23:48:43 +0530

    Skills added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -9,6 +9,8 @@ import Maven from "../assets/skills/maven.svg"
 import Java from "../assets/skills/java.png"
 import Git from "../assets/skills/git.png"
 import Github from "../assets/skills/github.png"
+import Postman from "../assets/skills/Postman.png"
+import Spring from "../assets/skills/Spring.png"
 import Tailwind from "../assets/skills/tailwind.png"
 import { useState } from "react"
 import SkillSet from "./SkillSet"
@@ -17,9 +19,10 @@ export default function Skills(){
     const[skillSet,setSkillSet] = useState([
         ["Programming Language", {logo : C,skilled : "80%"}, {logo :CPP,skilled : "80%"},{logo : Java,skilled : "80%"}],
         ["Front-End Technologies", {logo :HTML,skilled : "90%"}, {logo :CSS,skilled : "90%"}, {logo :JavaScript,skilled : "80%"}],
-        ["Framework", {logo :Maven,skilled:"50%"}, {logo :Bootstrap,skilled : "80%"},{logo :Tailwind,skilled : "80%"}],
-        ["Library", {logo :ReactImg,skilled : "60%"}],
+        ["Framework", {logo :Maven,skilled:"50%"}, {logo :Bootstrap,skilled : "80%"},{logo :Tailwind,skilled : "80%"},{logo:Spring,skilled:"60%"}],
+        ["Library", {logo :ReactImg,skilled : "70%"}],
         ["Version Control",{ logo :Git,skilled : "60%"}, {logo :Github,skilled : "60%"}],
+        ["Testing Tool",{logo : Postman,skilled:"70%"}]
     ])
     
     return(
